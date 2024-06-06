219cdef8bdce5b86d7a4009861ac9614575bc1dc
Martin Montes <martin11lrx@gmail.com>
2024-06-06 12:11:34 +0200

    Added helm index step

--- helm-index.sh
+++ helm-index.sh
@@ -1,44 +1,53 @@
 #!/bin/bash
 
-# Check for required commands
-if ! command -v yq &> /dev/null; then
-    echo "yq command not found, please install yq."
-    exit 1
+set -eo pipefail
+
+install_yq() {
+  if ! command -v yq &> /dev/null; then
+    echo "yq command not found, installing yq..."
+    sudo curl -sSLo /usr/local/bin/yq https://github.com/mikefarah/yq/releases/download/v4.13.4/yq_linux_amd64
+    sudo chmod +x /usr/local/bin/yq
+  fi
+}
+install_yq
+
+
+if [ -z "$REPO" ]; then 
+  echo "REPO environment variable is mandatory"
+  exit 1
+fi
+if [ -z "$BRANCH" ]; then 
+  echo "BRANCH environment variable is mandatory"
+  exit 1
+fi
+if [ -z "$BASE_URL" ]; then 
+  echo "BASE_URL environment variable is mandatory"
+  exit 1
+fi
+if [ -z "$GITHUB_TOKEN" ]; then
+  echo "GITHUB_TOKEN environment variable is mandatory"
+  exit 1
 fi
 
-if ! command -v gh &> /dev/null; then
-    echo "gh command not found, please install GitHub CLI."
-    exit 1
-fi
+export BASE_URL
+export GITHUB_TOKEN
 
-# Check if the correct number of parameters are passed
-if [ "$#" -ne 3 ]; then
-    echo "Usage: $0 <repository> <branch> <base_url>"
-    exit 1
-fi
-
-REPO=$1
-BRANCH=$2
-export BASE_URL=$3
-
-# Clone the repository and switch to the specified branch
 git clone https://github.com/$REPO.git
 cd $(basename "$REPO")
 git checkout $BRANCH
-git config user.email "martin11lrx@gmail.com"
-git config user.name "Martin Montes"
 
-# Update the index.yaml file using yq
 yq e -i '.entries.test[] |= . * {"urls": [env(BASE_URL) + .version]}' index.yaml
 
-# Commit the changes and push to a new branch
 NEW_BRANCH="update-index-$(date +%s)"
 git checkout -b $NEW_BRANCH
 git add index.yaml
 git commit -m "Update helm index.yaml with new URL"
 git push origin $NEW_BRANCH
 
-# Create a pull request using gh CLI
-gh pr create --title "Update helm index.yaml with new URL" --body "This PR updates the urls field in index.yaml with the new base URL." --base $BRANCH --head $NEW_BRANCH
+gh pr create \
+  --title "Update helm index.yaml" \
+  --body "This PR has been automatically raised after releasing a new helm chart." \
+  --base $BRANCH \
+  --head $NEW_BRANCH
 
 echo "Pull request created successfully."
